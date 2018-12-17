0b9b3a23aec4f6bd712453ae6a5e0bd72e0b5b5b
Amoyblack <634300012@qq.com>
2018-12-17 23:28:29 +0800

    Create A New Main Log

---
+++ config.lua
@@ -0,0 +1,23 @@
+
+C = {}
+
+C.NameFont = STANDARD_TEXT_FONT		--名字字体
+C.NameTextSize = 12					--名字大小
+C.CenterDetail = false              --居中显示血量与百分比
+
+-- 开启源生施法条后 下两条设定不会生效
+C.CastbarHeight = 5					--施法条高度
+C.CastBarIconSize = 18				--施法条法术图标大小
+
+-- 仇恨颜色
+-- 参数 r,g,b  格式: (255,0,255) --> (1,0,1)
+
+C.StableCol = {.9, .1, .4}  		--Tank稳定仇恨 / 默认暗粉色
+C.GainCol = {.9, .1, .9}			--Tank即将丢失仇恨 / 默认亮粉色
+C.HighCol = {.4, .1, .9}			--dps高仇恨（即将OT) / 默认紫色
+C.LowCol = {.1, .7, .9}				--dps低仇恨 (安全)/ 默认蓝色
+
+
+
+
+
